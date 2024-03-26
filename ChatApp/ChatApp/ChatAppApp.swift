6a80541f83bcd660271c4e5d4481650e3dc361d6
Diego Ignacio Núñez Hernández <diego.nunezh@ingenieria.unam.edu>
2024-03-26 12:11:57 +0100

    Start

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Diego Ignacio Nunez Hernandez on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
